301e1a4782c1fb4bac084a1a3dbb7a758699eebc
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-31 10:11:23 +0300

    Add files via upload

---
+++ Array/app.js
@@ -0,0 +1,47 @@
+// let baho = 100
+
+
+// if (baho >= 70) {
+//     console.log("Barakalla");
+// } else if  (baho >= 50) {
+//     console.log("Ortacha");
+// } else if  (baho == 50){
+//     console.log("Oqi ");
+// }
+
+
+
+
+
+// let studentscore = 70;
+
+// if (studentscore == 100) {
+//     console.log("Siz 5 oldingiz");
+// } else if (studentscore >= 80 && studentscore >= 70){
+//     console.log("Siz 4 oldingiz");
+// } else if (studentscore <= 50) {
+//     console.log("Oqishdan ketasan");
+// } else if (studentscore <= 49) {
+//     console.log("Yeqildin");
+// }
+
+
+// const Ism = ["Yunus", "Obidxojayev"];
+
+// console.log(Ism);
+
+// const meva = ["Olma", "Banan"];
+
+// console.log(meva);
+
+
+
+const hi = ["nimadir", "kimdir"];
+
+hi.push("Yunus");
+
+hi.unshift(30);
+
+hi.pop()
+
+console.log(hi);
